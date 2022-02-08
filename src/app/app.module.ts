772cc1efbf60087152938cb2b1a8a14b73dfb372
Aron-A <62127691+Aron-A@users.noreply.github.com>
2022-02-08 01:11:54 -0300

    Deploy Adjustaments - Version Home MVP 1.1 with Form Contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {MatButtonModule} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        NgbModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+
 })
+
+
 export class HomeComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`)
   public isCollapsed = true;
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  onSubmit(form: any){
+    console.log(form)
   }
+
+  constructor() {
+  }
+  ngOnInit() {
+
+  }
+
+
+
 
 }
